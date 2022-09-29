1b3e7935763c38f39d172aab3b148fac5283bb0d
renpk <rendykolot21@gmail.com>
2022-09-29 23:02:46 +0700

    TUGAS

--- percabangan.js
+++ percabangan.js
@@ -13,52 +13,55 @@
 12.  Capricorn: 22 Desember – 20 Januari */
 
 var tanggal = prompt("Tanggal:");
-var bulan = prompt("Bulan:");
-    // Aquarius
-    if(tanggal >=21   && bulan == "januari" || tanggal <=19 && bulan == "februari"){
+var bulan = prompt("Bulan:"); 
+
+// Aquarius
+    if(tanggal >=21   && bulan == "januari" || tanggal == 1 <=19 && bulan == "februari"){
         document.write("Zodiak yang di temukan yaitu : <b>Aquarius</b>");
     }
     // Pisces
-    if(tanggal >=20   && bulan == "februari" || tanggal <=20 && bulan == "maret"){
+    if(tanggal >=20   && bulan == "februari" || tanggal == 1 <=20 && bulan == "maret"){
         document.write("Zodiak yang di temukan yaitu : <b>Pisces</b>");
     } 
     // Aries
-    if(tanggal >=21   && bulan == "maret" || tanggal <=20 && bulan == "april"){
+    if(tanggal >=21   && bulan == "maret" || tanggal == 1 <=20 && bulan == "april"){
         document.write("Zodiak yang di temukan yaitu : <b>Aries</b>");
     }
     // Taurus
-    if(tanggal >=21   && bulan == "april" || tanggal <=21 && bulan == "mei"){
+    if(tanggal >=21   && bulan == "april" || tanggal == 1 <=21 && bulan == "mei"){
         document.write("Zodiak yang di temukan yaitu : <b>Taurus</b>");
     }
     // Gemini
-    if(tanggal >=22   && bulan == "mei" || tanggal <=21 && bulan == "juni"){
+    if(tanggal >=22   && bulan == "mei" || tanggal == 1 <=21 && bulan == "juni"){
         document.write("Zodiak yang di temukan yaitu : <b>Gemini</b>");
     }
     // Cancer
-    if(tanggal >=22   && bulan == "juni" || tanggal <=23 && bulan == "juli"){
+    if(tanggal >=22   && bulan == "juni" || tanggal == 1 <=23 && bulan == "juli"){
         document.write("Zodiak yang di temukan yaitu : <b>Cancer</b>");
     }
     // Leo
-    if(tanggal >=24   && bulan == "juli" || tanggal <=23 && bulan == "agustus"){
+    if(tanggal >=24   && bulan == "juli" || tanggal == 1 <=23 && bulan == "agustus"){
         document.write("Zodiak yang di temukan yaitu : <b>Leo</b>");
     }
     // Virgo
-    if(tanggal >=24   && bulan == "agustus" || tanggal <=23 && bulan == "september"){
+    if(tanggal >=24   && bulan == "agustus" || tanggal == 1 <=23 && bulan == "september"){
         document.write("Zodiak yang di temukan yaitu : <b>Virgo</b>");
     }
     // Libra
-    if(tanggal >=24   && bulan == "september" || tanggal <=23 && bulan == "oktober"){
+    if(tanggal >=24   && bulan == "september" || tanggal == 1 <=23 && bulan == "oktober"){
         document.write("Zodiak yang di temukan yaitu : <b>Libra</b>");
     }
     // Scorpio
-    if(tanggal >=24   && bulan == "oktober" || tanggal <=22 && bulan == "november"){
+    if(tanggal >=24   && bulan == "oktober" || tanggal == 1 <=22 && bulan == "november"){
         document.write("Zodiak yang di temukan yaitu : <b>Scorpio</b>");
     }
     // Sagitarius
-    if(tanggal >=23   && bulan == "november" || tanggal <=21 && bulan == "desember"){
+    if(tanggal >=23   && bulan == "november" || tanggal == 1 <=21 && bulan == "desember"){
         document.write("Zodiak yang di temukan yaitu : <b>Sagitarius</b>");
     }
     // Capicorn
-    if(tanggal >=22   && bulan == "desember" || tanggal <=20 && bulan == "januari"){
+    if(tanggal >=22   && bulan == "desember" || tanggal == 1 <=20 && bulan == "januari"){
         document.write("Zodiak yang di temukan yaitu : <b>Capicorn</b>");
+    }else{
+        alert('tidak valid');
     }
